3056fb23c628a9d6529eba00ae89763da2f7ab2d
Kiona <kiona.coats@us.af.mil>
2022-12-23 12:55:27 -0600

    Completed section 3/20. Learned components, props, state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-// Main App Component
+import { useState } from "react";
+import Header from "./components/Header";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackData from "./data/FeedbackData";
+import FeedbackStats from "./components/FeedbackStats";
 
-function App(){
-    return (
-        <div className='container'>
-            <h1>My App</h1>
-        </div>
-    
-    );
+function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
+
+  const deleteFeedback = (id) => {
+    if (window.confirm("Are you sure you want to delete this entry?")) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
+
+  return (
+    <>
+      <Header />
+      <div className='container'>
+        <FeedbackStats feedback={feedback} />
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
