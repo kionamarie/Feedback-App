9473d497e7e87b3c172646803e698b7a420c65c2
Kiona <kiona.coats@us.af.mil>
2022-12-27 10:31:38 -0600

    Added Feedback Form and Button components. Add validation to feedback input.

---
+++ src/components/FeedbackForm.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import Card from "./shared/Card";
+import Button from "./shared/Button";
+
+function FeedbackForm() {
+  const [text, setText] = useState("");
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState("");
+
+  const handleTextChange = (e) => {
+    // validation
+    if (text === "") {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (text !== "" && text.trim().length <= 10) {
+      setBtnDisabled(true);
+      setMessage("Must be atleast 10 characters");
+    } else {
+      setMessage(null);
+      setBtnDisabled(false);
+    }
+    setText(e.target.value);
+  };
+
+  return (
+    <Card>
+      <form>
+        <h2>How would you rate your service with us?</h2>
+        {/* add rating select component here */}
+        <div className='input-group'>
+          <input
+            onChange={handleTextChange}
+            type='text'
+            placeholder='Leave feedback'
+            value={text}
+          />
+          <Button type='submit' isDisabled={btnDisabled}>
+            Send
+          </Button>
+        </div>
+        {message && <div className='message'>{message}</div>}
+      </form>
+    </Card>
+  );
+}
+
+export default FeedbackForm;
