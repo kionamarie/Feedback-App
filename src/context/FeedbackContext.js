90d135a1cec8a86eaea9ef6d60c52d2a57e1c87d
Kiona <kiona.coats@us.af.mil>
2022-12-28 10:28:45 -0600

    Feedback data can now be fetched from db.json

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,36 +1,50 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([
-    { id: 1, text: "This is feedback item 1", rating: 10 },
-    { id: 2, text: "This is feedback item 2", rating: 9 },
-    { id: 3, text: "This is feedback item 3", rating: 8 },
-  ]);
-
+  const [isLoading, setIsLoading] = useState(true);
+  const [feedback, setFeedback] = useState([]);
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false,
   });
 
+  useEffect(() => {
+    fetchFeedback();
+  }, []);
+
+  // Fetches feedback
+  const fetchFeedback = async () => {
+    const response = await fetch(
+      "http://localhost:5000/feedback?_sort=id&_order=desc"
+    );
+    const data = await response.json();
+
+    setFeedback(data);
+    setIsLoading(false);
+  };
+
+  // Deletes feedback
   const deleteFeedback = (id) => {
     if (window.confirm("Are you sure you want to delete this entry?")) {
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
 
+  // Adds feedback
   const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4();
     setFeedback([newFeedback, ...feedback]);
   };
+
   // Sets item to be updated
   const editFeedback = (item) => {
     setFeedbackEdit({ item, edit: true });
   };
 
-  // Update feedback item
+  // Updats feedback item
   const updateFeedback = (id, updItem) => {
     setFeedback(
       feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item))
@@ -42,6 +56,7 @@ export const FeedbackProvider = ({ children }) => {
       value={{
         feedback,
         feedbackEdit,
+        isLoading,
         deleteFeedback,
         addFeedback,
         editFeedback,
